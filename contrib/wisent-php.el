e9b51bd127079473d00f3ea43844ff35d5fbeb78
steven.remot@telecom-paristech.fr <steven.remot@telecom-paristech.fr>
2014-08-06 13:48:59 +0200

    PHP: Implement multiple aliases in one USE

--- contrib/wisent-php.el
+++ contrib/wisent-php.el
@@ -116,7 +116,7 @@ Use the alternate LALR(1) parser."
   ;; Setup phpdoc stuff
   ;;(semantic-php-doc-setup))
 
-	
+
 (defun wisent-php-expand-tag (tag)
   "Expand TAG into a list of equivalent tags, or nil.
 Expand multiple variable declarations in the same statement, that is
@@ -124,8 +124,9 @@ tags of class `variable' whose name is equal to a list of elements of
 the form (NAME START . END).  NAME is a variable name.  START and END
 are the bounds in the declaration, related to this variable NAME."
   (let (elts elt clone start end xpand)
-    (when (and (eq 'variable (semantic-tag-class tag))
-               (consp (setq elts (semantic-tag-name tag))))
+    (cond
+     ((and (eq 'variable (semantic-tag-class tag))
+           (consp (setq elts (semantic-tag-name tag))))
       ;; There are multiple names in the same variable declaration.
       (while elts
         ;; For each name element, clone the initial tag and give it
@@ -138,8 +139,22 @@ are the bounds in the declaration, related to this variable NAME."
               xpand (cons clone xpand))
         ;; Set the bounds of the cloned tag with those of the name
         ;; element.
-        (semantic-tag-set-bounds clone start end))
-      xpand)))
+        (semantic-tag-set-bounds clone start end)))
+     ((eq 'alias (semantic-tag-class tag))
+      (let ((names (semantic-tag-name tag))
+            (defs (semantic-tag-alias-definition tag)))
+        (when (and (consp names) (consp defs))
+          (dotimes (i (length names))
+            (let* ((elt (nth i names))
+                   (def (nth i defs))
+                   (name (car elt))
+                   (region (cdr elt)))
+              (setq clone (semantic-tag-new-alias name "alias" def)
+                    xpand (cons clone xpand)
+                    start (car region)
+                    end (cdr region))
+              (semantic-tag-set-bounds clone start end)))))))
+    xpand))
 
 ;;;###autoload
 (add-hook 'php-mode-hook #'wisent-php-default-setup)
